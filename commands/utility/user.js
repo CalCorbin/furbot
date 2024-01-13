5ef601a19b2615ac6634290561f5e8cf1dfc0a0f
ccorbin <calcorbin@gmail.com>
2024-01-13 07:52:31 -0600

    fix: Remove uppercase chars from slash commands

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,9 +1,9 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("userInfo")
-    .setDescription("Provides information about the user."),
+    .setName('userinfo')
+    .setDescription('Provides information about the user.'),
   async execute(interaction) {
     // interaction.user is the object representing the User who ran the command
     // interaction.member is the GuildMember object, which represents the user in the specific guild
