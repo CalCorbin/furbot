458f65f5c498fe4321f448e62042662c08a62f4f
ccorbin <calcorbin@gmail.com>
2024-01-13 07:23:56 -0600

    Add prettier

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+require("dotenv").config();
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
   }
 });
 
